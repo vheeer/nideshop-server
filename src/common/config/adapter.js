aaa2337330fc3f717302af41ce6cb4bdac181eea
vheeer <vheeer@126.com>
2018-09-02 18:59:53 +0800

    添加river库

--- src/common/config/adapter.js
+++ src/common/config/adapter.js
@@ -132,6 +132,17 @@ exports.model = {
     user: 'root',
     password: ';Classmate1133',
     dateStrings: true
+  },
+  river: {  
+    handle: mysql,
+    database: 'nideshop_river',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
   }
 };
 
